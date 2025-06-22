602c9f5c9987f01c6de2efc3fc05d61ef400db43
Filip Sokołowski <123462364+Sok205@users.noreply.github.com>
2025-06-22 11:09:16 +0200

    Graphs. Dijstkra. Bellman

--- DSA/test3/src/lib.rs
+++ DSA/test3/src/lib.rs
@@ -55,17 +55,28 @@ pub mod hash_tables {
 
 pub mod graphs {
     use std::collections::HashMap;
+    use std::fs::File;
+    use std::io::Write;
+    use std::process::Command;
+
     pub struct Node<T> {
-        value: T,
+        pub value: T,
     }
 
     pub struct Graph<T> {
-        nodes: Vec<Node<T>>,
-        adj: Vec<Vec<usize>>,
+        pub nodes: Vec<Node<T>>,
+        pub adj: Vec<Vec<usize>>,
+        pub edges: Vec<Edge>,
+    }
+    #[derive(Clone)]
+    pub struct Edge {
+        pub(crate) src: usize,
+        pub(crate) dest: usize,
+        pub(crate) weight: i32,
     }
     impl<T: std::fmt::Display> Graph<T>{
         pub fn new() -> Self {
-            Graph { nodes: Vec::new(), adj: Vec::new() }
+            Graph { nodes: Vec::new(), adj: Vec::new(), edges: vec![] }
         }
 
         pub fn add_node(&mut self, value: T) {
@@ -73,9 +84,14 @@ pub mod graphs {
             self.adj.push(Vec::new());
         }
 
-        pub fn add_edge(&mut self, u: usize, v: usize) {
+        pub fn add_edge(&mut self, u: usize, v: usize, weight: i32) {
             self.adj[u].push(v);
             self.adj[v].push(u);
+            self.edges.push(Edge { src: u, dest: v, weight });
+        }
+        
+        pub fn add_edge_simple(&mut self, u: usize, v: usize) {
+            self.add_edge(u, v, 0);
         }
 
         pub fn display(&self) {
@@ -169,12 +185,7 @@ pub mod graphs {
             println!();
         }
     }
-
-    use std::fs::File;
-    use std::io::Write;
-    use std::process::Command;
-
-    impl<T: std::fmt::Display> Graph<T> {
+    impl<T: std::fmt::Display + Clone> Graph<T> {
         pub fn to_dot(&self) -> String {
             let mut dot = String::from("graph G {\n");
 
@@ -198,9 +209,11 @@ pub mod graphs {
         }
 
         pub fn visualize(&self, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+            std::fs::create_dir_all("graphs")?;
+
             let dot_content = self.to_dot();
-            let dot_file = format!("{}.dot", filename);
-            let png_file = format!("{}.png", filename);
+            let dot_file = format!("graphs/{}.dot", filename);
+            let png_file = format!("graphs/{}.png", filename);
 
             let mut file = File::create(&dot_file)?;
             file.write_all(dot_content.as_bytes())?;
@@ -212,21 +225,171 @@ pub mod graphs {
             println!("Graph saved as {}", png_file);
             Ok(())
         }
+
+        pub fn sort_adj(&mut self){
+            for neighbors in &mut self.adj {
+                neighbors.sort();
+            }
+        }
+
+        pub fn kruskal(&mut self) -> Self {
+            let mut mst = Graph::new();
+
+            for node in &self.nodes {
+                mst.add_node(node.value.clone());
+            }
+
+            let mut sorted_edges = self.edges.clone();
+            sorted_edges.sort_by_key(|e| e.weight);
+
+            let mut parent: Vec<usize> = (0..self.nodes.len()).collect();
+
+            fn find(parent: &mut Vec<usize>, u: usize) -> usize {
+                if parent[u] != u {
+                    parent[u] = find(parent, parent[u]);
+                }
+                parent[u]
+            }
+
+            fn union(parent: &mut Vec<usize>, u: usize, v: usize) {
+                let root_u = find(parent, u);
+                let root_v = find(parent, v);
+                parent[root_u] = root_v;
+            }
+
+            for edge in sorted_edges {
+                let u = find(&mut parent, edge.src);
+                let v = find(&mut parent, edge.dest);
+
+                if u != v {
+                    mst.add_edge(edge.src, edge.dest, edge.weight);
+                    union(&mut parent, u, v);
+                }
+            }
+            println!("Minimum Spanning Tree:");
+            for edge in &mst.edges {
+                println!("{} -- {} (weight: {})", mst.nodes[edge.src].value, mst.nodes[edge.dest].value, edge.weight);
+            }
+
+        mst
+        }
+
+        pub fn prims(&mut self) -> Self{
+            let mut mst = Graph::new();
+            for node in &self.nodes {
+                mst.add_node(node.value.clone());
+            }
+
+            let mut visited = vec![false; self.nodes.len()];
+            let mut min_heap = std::collections::BinaryHeap::new();
+
+            visited[0] = true;
+            for &neighbor in &self.adj[0] {
+                min_heap.push((0, 0, neighbor));
+            }
+
+            while let Some((weight, src, dest)) = min_heap.pop() {
+                if visited[dest] {
+                    continue;
+                }
+                visited[dest] = true;
+                mst.add_edge(src, dest, weight);
+
+                for &next_neighbor in &self.adj[dest] {
+                    if !visited[next_neighbor] {
+                        min_heap.push((weight + 1, dest, next_neighbor)); // Assuming weight is 1 for simplicity
+                    }
+                }
+            }
+
+            println!("Minimum Spanning Tree using Prim's Algorithm:");
+            for edge in &mst.edges {
+                println!("{} -- {} (weight: {})", mst.nodes[edge.src].value, mst.nodes[edge.dest].value, edge.weight);
+            }
+
+            mst
+        }
+
+        pub fn dijkstra(&self, start: usize) -> Vec<i32> {
+            use std::collections::BinaryHeap;
+            use std::cmp::Reverse;
+
+            let mut distances = vec![i32::MAX; self.nodes.len()];
+            distances[start] = 0;
+
+            let mut priority_queue = BinaryHeap::new();
+            priority_queue.push(Reverse((0, start)));
+
+            while let Some(Reverse((current_dist, current_vertex))) = priority_queue.pop() {
+                if current_dist > distances[current_vertex] {
+                    continue;
+                }
+
+                for &neighbor in &self.adj[current_vertex] {
+                    let edge = self.edges.iter()
+                        .find(|e| (e.src == current_vertex && e.dest == neighbor) ||
+                            (e.src == neighbor && e.dest == current_vertex))
+                        .unwrap();
+
+                    let weight = edge.weight;
+                    let distance = current_dist + weight;
+
+                    if distance < distances[neighbor] {
+                        distances[neighbor] = distance;
+                        priority_queue.push(Reverse((distance, neighbor)));
+                    }
+                }
+            }
+
+            distances
+        }
+
+        pub fn bellman_ford(&self, source: usize) -> Result<Vec<i32>, &'static str> {
+            let vertices = self.nodes.len();
+
+            let mut distances = vec![i32::MAX; vertices];
+            distances[source] = 0;
+
+            for _ in 0..vertices - 1 {
+                for edge in &self.edges {
+                    if distances[edge.src] == i32::MAX {
+                        continue;
+                    }
+
+                    let potential_distance = match distances[edge.src].checked_add(edge.weight) {
+                        Some(sum) => sum,
+                        None => continue,
+                    };
+
+                    if potential_distance < distances[edge.dest] {
+                        distances[edge.dest] = potential_distance;
+                    }
+                }
+            }
+
+            for edge in &self.edges {
+                if distances[edge.src] != i32::MAX
+                    && distances[edge.src] + edge.weight < distances[edge.dest] {
+                    return Err("Graph contains a negative weight cycle");
+                }
+            }
+
+            Ok(distances)
+        }
     }
 
     pub fn graph_main(){
-        let mut graph = Graph { nodes: Vec::new(), adj: vec![] };
-
+        let mut graph = Graph { nodes: Vec::new(), adj: vec![], edges: vec![] };
         graph.add_node(1);
         graph.add_node(2);
         graph.add_node(3);
         graph.add_node(4);
 
-        graph.add_edge(0, 1);
-        graph.add_edge(1, 2);
-        graph.add_edge(2, 3);
-        graph.add_edge(3, 0);
-        graph.add_edge(0, 2);
+        graph.add_edge_simple(1, 2);
+        graph.add_edge_simple(0, 1);
+        graph.add_edge_simple(2, 3);
+        graph.add_edge_simple(3, 0);
+        graph.add_edge_simple(0, 2);
 
         println!("DFS Traversal:");
         graph.dfs(0);
@@ -237,4 +400,98 @@ pub mod graphs {
         graph.visualize("graph.dot").unwrap();
     }
 
+}
+
+pub mod mst{
+    use crate::graphs::{Graph, Node};
+    pub fn mst_main(){
+        let mut mstGraph = Graph::new();
+        mstGraph.add_node(1);
+        mstGraph.add_node(2);
+        mstGraph.add_node(3);
+        mstGraph.add_node(4);
+
+        mstGraph.add_edge(0, 1, 10);
+        mstGraph.add_edge(0, 2, 6);
+        mstGraph.add_edge(0, 3, 5);
+        mstGraph.add_edge(1, 3, 15);
+        mstGraph.add_edge(2, 3, 4);
+
+        let minimum_spanning_tree = mstGraph.kruskal();
+        mstGraph.visualize("mst_before.dot").unwrap();
+        minimum_spanning_tree.visualize("mst_graph.dot").unwrap();
+
+        let minmum_spanning_tree_prims = mstGraph.prims();
+        minmum_spanning_tree_prims.visualize("mst_prims.dot").unwrap();
+    }
+}
+
+pub mod dijkstra {
+    use crate::graphs::{Graph, Node};
+
+    pub fn dijkstra_main() {
+        let mut graph = Graph::new();
+        graph.add_node(1);
+        graph.add_node(2);
+        graph.add_node(3);
+        graph.add_node(4);
+
+        graph.add_edge(0, 1, 10);
+        graph.add_edge(0, 2, 6);
+        graph.add_edge(0, 3, 5);
+        graph.add_edge(1, 3, 15);
+        graph.add_edge(2, 3, 4);
+
+        let distances = graph.dijkstra(0);
+        println!("Dijkstra's Algorithm Shortest Distances from Node 0:");
+        for (i, distance) in distances.iter().enumerate() {
+            println!("Distance from Node 0 to Node {}: {}", i, distance);
+        }
+    }
+}
+
+pub mod bellman_ford {
+    use crate::graphs::{Graph, Node};
+
+    pub fn bellman_ford_main() {
+        let mut graph = Graph::new();
+
+        for i in 0..6 {
+            graph.add_node(i);
+        }
+
+        graph.add_edge(0, 1, 5);
+        graph.add_edge(0, 2, 7);
+        graph.add_edge(1, 2, 3);
+        graph.add_edge(1, 3, 4);
+        graph.add_edge(1, 4, 6);
+        graph.add_edge(3, 4, -1);
+        graph.add_edge(3, 5, 2);
+        graph.add_edge(4, 5, -3);
+
+        match graph.bellman_ford(0){
+            Ok(distances) => {
+                println!("Bellman-Ford Algorithm Shortest Distances from Node 0:");
+                for (i, distance) in distances.iter().enumerate() {
+                    println!("Distance from Node 0 to Node {}: {}", i, distance);
+                }
+                let mut shortest_paths_graph = Graph::new();
+                for i in 0..6 {
+                    shortest_paths_graph.add_node(i);
+                }
+                for edge in &graph.edges {
+                    if distances[edge.src] != i32::MAX &&
+                        distances[edge.dest] != i32::MAX &&
+                        distances[edge.dest] == distances[edge.src] + edge.weight {
+                        shortest_paths_graph.add_edge(edge.src, edge.dest, edge.weight);
+                    }
+                }
+                shortest_paths_graph.visualize("bellman_ford_shortest_paths.dot").unwrap();
+
+            },
+            Err(err) => println!("{}", err),
+        }
+
+        graph.visualize("bellman_ford_graph.dot").unwrap();
+    }
 }
--- DSA/test3/src/main.rs
+++ DSA/test3/src/main.rs
@@ -12,7 +12,26 @@ fn main() {
     test3::graphs::adjaceny_matrix_main();
     println!("Adjacency Matrix theory: An adjacency matrix is a 2D array where the element at row i and column j indicates whether there is an edge between vertex i and vertex j. This representation is efficient for dense graphs, where the number of edges is close to the square of the number of vertices, as it allows for quick edge existence checks but uses more space compared to an adjacency list.");
 
-    println!("Graph");
+    println!("Graphs");
     test3::graphs::graph_main();
+
+    println!("Graph theory: A graph is a collection of nodes (or vertices) and edges that connect pairs of nodes. Graphs can be directed or undirected, weighted or unweighted, and can represent various structures such as networks, trees, and more. They are used in many applications, including social networks, transportation systems, and computer networks.");
+    println!("BFS uses a queue to explore nodes level by level, ensuring that all nodes at the current depth are visited before moving deeper. It is useful for finding the shortest path in unweighted graphs. DFS uses a stack (or recursion) to explore as far as possible along each branch before backtracking, which can be more memory efficient for deep graphs but may not find the shortest path in unweighted graphs.");
+    println!("DFS uses LIFO (Last In, First Out) structure, typically implemented with a stack or recursion, to explore as deep as possible into the graph before backtracking. BFS uses FIFO (First In, First Out) structure, typically implemented with a queue, to explore all neighbors at the present depth prior to moving on to nodes at the next depth level.");
+
+    println!("MST");
+    test3::mst::mst_main();
+
+    println!("MST theory: A Minimum Spanning Tree (MST) is a subset of the edges of a connected, undirected graph that connects all the vertices together without any cycles and with the minimum possible total edge weight. MST algorithms like Prim's and Kruskal's are used to find such trees efficiently. They are useful in network design, clustering, and other applications where minimizing cost while maintaining connectivity is important.");
+    println!("Prim's algorithm starts with a single vertex and grows the MST by adding the smallest edge that connects a vertex in the MST to a vertex outside it, ensuring that no cycles are formed. Kruskal's algorithm sorts all edges and adds them one by one to the MST, ensuring that no cycles are formed, until all vertices are connected. Both algorithms have different use cases and performance characteristics depending on the graph structure.");
+    println!("Kruskal's algorithm uses a disjoint-set data structure to efficiently manage and merge sets of vertices, ensuring that no cycles are formed while adding edges. It sorts the edges by weight and adds them in increasing order, checking for cycles using the disjoint-set structure. Prim's algorithm, on the other hand, grows the MST from a starting vertex by always adding the smallest edge that connects a vertex in the MST to a vertex outside it, using a priority queue to efficiently find the next edge to add.");
+
+    println!("Dijksta's");
+    test3::dijkstra::dijkstra_main();
+    println!("Dijkstra's algorith theory: Dijkstra's algorithm is a graph search algorithm that finds the shortest path from a source vertex to all other vertices in a weighted graph with non-negative edge weights. It uses a priority queue to explore the closest unvisited vertex, updating the shortest known distances to its neighbors. The algorithm guarantees that once a vertex is visited, the shortest path to it has been found. It is widely used in routing and navigation systems.");
+
+    println!("Bellman-Ford Algorithm");
+    test3::bellman_ford::bellman_ford_main();
+    println!("Bellman-Ford algorithm theory: The Bellman-Ford algorithm is a graph search algorithm that computes the shortest paths from a single source vertex to all other vertices in a weighted graph, allowing for negative edge weights. It works by repeatedly relaxing the edges, updating the shortest known distances, and can detect negative weight cycles. It is useful in scenarios where graphs may have negative weights, such as in financial networks or certain optimization problems.");
 }
 
